1be90b84e2bab239ba8be0498c585b1cd26d9205
Eddy Ekofo <eddy.ekofo@amadeus.com>
2024-05-28 08:53:22 +0000

    Change session keymaps

--- lua/modules/mini/sessions.lua
+++ lua/modules/mini/sessions.lua
@@ -6,18 +6,18 @@ return {
   event = "VimEnter",
   keys = {
     {
-      "<leader>sw",
+      "<leader>Sw",
       '<cmd>:lua MiniSessions.write((vim.fn.getcwd():gsub("/", "_")))<CR>',
       desc = "Session Write",
     },
     {
-      "<leader>sW",
+      "<leader>SW",
       ':lua MiniSessions.write((vim.fn.getcwd():gsub("/", "_")))',
       desc = "Session Write Custom",
     },
-    { "<leader>ss", "<cmd>:lua MiniSessions.select()<CR>", desc = "Session Select" },
+    { "<leader>Ss", "<cmd>:lua MiniSessions.select()<CR>", desc = "Session Select" },
     {
-      "<leader>sd",
+      "<leader>Sd",
       '<cmd>:lua MiniSessions.delete((vim.fn.getcwd():gsub("/", "_")))<CR>',
       desc = "Session Delete",
     },
@@ -32,10 +32,12 @@ return {
     --   --   vim.cmd("bd! " .. tostring(buf))
     --   -- end
     -- end
+
+    --  TODO: 2024-05-27 - Make session per cwd
     return {
       autoread = true,
       directory = vim.fn.stdpath "state" .. "/sessions/",
-      file = "session.vim",
+      file = vim.fn.getcwd() .. "session.vim",
       -- Whether to force possibly harmful actions (meaning depends on function)
       force = { read = false, write = true, delete = true },
       hooks = {
@@ -46,5 +48,37 @@ return {
       },
     }
   end,
-  config = true,
+  config = function()
+    local H = {}
+    local M = {}
+
+    require("mini.sessions").setup()
+
+    M.save = function()
+      local res = H.get_session_from_user "Save session as: "
+      if res ~= nil then
+        MiniSessions.write(res)
+      end
+    end
+
+    -- For autocompletion of session name
+    -- Config._session_complete = function(arg_lead)
+    --   return vim.tbl_filter(function(x)
+    --     return x:find(arg_lead, 1, true) ~= nil
+    --   end, vim.tbl_keys(MiniSessions.detected))
+    -- end
+
+    H.get_session_from_user = function(prompt)
+      local completion = "customlist,v:lua.Config._session_complete"
+      local ok, res = pcall(vim.fn.input, {
+        prompt = prompt,
+        cancelreturn = false,
+        completion = completion,
+      })
+      if not ok or res == false then
+        return nil
+      end
+      return res
+    end
+  end,
 }
