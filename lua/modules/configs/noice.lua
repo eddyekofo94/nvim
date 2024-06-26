4423cea596301182169741bf6b295b3f8a7a5ad2
Eddy Ekofo <eddy.ekofo@amadeus.com>
2024-06-26 08:15:41 +0000

    Added some config

--- lua/modules/configs/noice.lua
+++ lua/modules/configs/noice.lua
@@ -23,6 +23,15 @@ require("noice").setup {
     view_warn = "notify", -- view for warnings
     view_history = "messages", -- view for :messages
     view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+    filter = {
+      any = {
+        { event = "notify" },
+        { error = true },
+        { warning = true },
+        { event = "msg_show", kind = { "" } },
+        { event = "lsp", kind = "message" },
+      },
+    },
   },
   popupmenu = {
     enabled = true, -- enables the Noice popupmenu UI
