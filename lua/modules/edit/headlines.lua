dcf09caeae27bd8345b4ae389918f68f92098c68
Eddy Ekofo <eddy.ekofo@amadeus.com>
2024-06-27 17:16:27 +0000

    Added new configs

---
+++ lua/modules/edit/headlines.lua
@@ -0,0 +1,28 @@
+return { -- highlight markdown headings and code blocks etc.
+  "lukas-reineke/headlines.nvim",
+  ft = "markdown",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  config = function()
+    require("headlines").setup {
+      quarto = {
+        query = vim.treesitter.query.parse(
+          "markdown",
+          [[
+                (fenced_code_block) @codeblock
+                ]]
+        ),
+        codeblock_highlight = "CodeBlock",
+        treesitter_language = "markdown",
+      },
+      markdown = {
+        query = vim.treesitter.query.parse(
+          "markdown",
+          [[
+                (fenced_code_block) @codeblock
+                ]]
+        ),
+        codeblock_highlight = "CodeBlock",
+      },
+    }
+  end,
+}
