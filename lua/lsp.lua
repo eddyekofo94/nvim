8352d3e1ed551a322e8558e91a2fa873820c738d
Eddy Ekofo <ekofoeddy@gmail.com>
2025-04-02 00:11:58 +0200

    Improved lsp.config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -108,5 +108,22 @@ vim.lsp.config("*", {
 
 vim.diagnostic.config {
   virtual_text = false,
-  virtual_lines = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = icons.Diamond,
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "󰋼",
+      [vim.diagnostic.severity.HINT] = "󰌵"
+    },
+  },
+  float = {
+    border = "rounded",
+    format = function(d)
+      return ("%s (%s) [%s]"):format(d.message, d.source, d.code or d.user_data.lsp.code)
+    end,
+  },
+  underline = true,
+  jump = {
+    float = true,
+  },
 }
