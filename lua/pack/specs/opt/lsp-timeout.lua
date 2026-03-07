30422162cb75c17f049b69eaeb4a12b487990a65
Eddy Ekofo <ekofoeddy@gmail.com>
2026-03-07 01:31:42 +0100

    fix: lsp-timeout load after lspconfig is available

--- lua/pack/specs/opt/lsp-timeout.lua
+++ lua/pack/specs/opt/lsp-timeout.lua
@@ -3,10 +3,15 @@ return {
   src = 'https://github.com/hinell/lsp-timeout.nvim',
   data = {
     event = 'LspAttach',
+    dependencies = { 'neovim/nvim-lspconfig' },
     postload = function()
+      local ok, lspconfig = pcall(require, 'lspconfig')
+      if not ok then
+        return
+      end
       require('lsp-timeout').setup({
         timeout = 3000,
-        notify = true,
+        notify = false,
       })
     end,
   },
